4f053ba32c1c5baf6f2324527085f5a67f674938
Yapibe <Pickholz.yair@gmail.com>
2024-12-16 18:19:26 +0200

    Update pipeline files and create pathways file

---
+++ pipeline/create_pathways_file.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+# Get the project root directory (assuming your script is in the pipeline folder)
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+new_file_path = os.path.join(project_root, "pipeline", "Data", "H_sapiens", "pathways", "MSigDB_c2_CP.gmt")
+output_folder = os.path.join(project_root, "pipeline", "Data", "H_sapiens", "pathways")
+os.makedirs(output_folder, exist_ok=True)  # Create the folder if it doesn't exist
+output_file_path = os.path.join(output_folder, "MSigDB_c2_CP")
+
+new_data = []
+
+with open(new_file_path, 'r') as file:
+    for line in file:
+        parts = line.strip().split('\t')
+        pathway_name = parts[0]
+        genes = parts[2:]  # Skip the URL part
+        new_data.append([pathway_name] + genes)
+
+# Convert to DataFrame
+new_pathways_df = pd.DataFrame(new_data)
+
+# Save the DataFrame to a TSV file in the specified folder
+new_pathways_df.to_csv(output_file_path, index=False, header=False, sep='\t')
+
+# Display the first few rows to verify the structure
+new_pathways_df.head()
